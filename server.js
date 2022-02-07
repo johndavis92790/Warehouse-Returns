b0dca155f437f4effbc61400fcbd7f5fb5482dee
danieljardine04 <danogaryjardine@gmail.com>
2022-02-07 14:39:04 -0700

    Finished mock up HTML for office page and login page

--- server.js
+++ server.js
@@ -2,5 +2,11 @@ const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
