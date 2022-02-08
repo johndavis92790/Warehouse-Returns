600163ae6eb4eaca8b6d80faacf2d46b68414d76
johndavis <johndavis92790@gmail.com>
2022-02-08 11:20:46 -0700

    working on public js files

--- server.js
+++ server.js
@@ -16,36 +16,36 @@
 
 const path = require("path");
 const express = require("express");
-const session = require("express-session");
+// const session = require("express-session");
 // const exphbs = require("express-handlebars");
 
 const app = express();
 app.use(express.static('./test-htmls'));
 
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+// const exphbs = require('express-handlebars');
+// const hbs = exphbs.create({});
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/connection");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
+// const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-const sess = {
-  secret: "Super secret secret",
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize,
-  }),
-};
+// const sess = {
+//   secret: "Super secret secret",
+//   cookie: {},
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize,
+//   }),
+// };
 
-app.use(session(sess));
+// app.use(session(sess));
 
 // const helpers = require("./utils/helpers");
 
